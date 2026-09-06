17b079bc1d9ed19a3b272ae2e98a71e52bb16a60
Omirbay Akseleu <omirbajakseleu5@gmail.com>
2026-09-06 15:32:43 +0600

    Update gdn2_bwd.py

--- atomic_ops/gdn2_bwd.py
+++ atomic_ops/gdn2_bwd.py
@@ -253,20 +253,6 @@ def _kernel_b4_body(q_ref, k_ref, b_ref, g_ref, daqk_ref, dakk_ref,
     db_ref[0, 0, 0] = jnp.zeros_like(k_full)
     dgc_ref[0, 0, 0] = jnp.zeros_like(g_raw)
 
-    if use_centering:
-        n_mid = bt // 2
-        gn = gc[n_mid]
-        # WIP / KNOWN BROKEN: this accumulator exists so the loop below has
-        # somewhere to write dgn contributions, but the write-back into
-        # dgc_ref[..., n_mid] at the end of this function is INCOMPLETE --
-        # it does not correctly account for every path through which gn
-        # affects the forward output (see KNOWN_LIMITATIONS.md, "Bugs 2/3").
-        # This is why KernelConfig.__post_init__ hard-refuses
-        # use_centering=True for any config used by
-        # gdn2_pallas_forward_trainable. Do NOT treat the presence of this
-        # accumulator as evidence the gradient path is fixed -- it is not.
-        dgn_acc = jnp.zeros_like(gn)
-
     for si in range(n_sub):
         for sj in range(si + 1):
             i0, i1 = si * bc, (si + 1) * bc
@@ -289,67 +275,27 @@ def _kernel_b4_body(q_ref, k_ref, b_ref, g_ref, daqk_ref, dakk_ref,
                 dM_qk = dM_qk * causal
                 dM_kk = dM_kk * strict
 
-            if use_centering:
-                gq_i_raw = gc_i - gn[None, :]
-                gk_j_raw = gn[None, :] - gc_j
-                gq_i = jnp.clip(gq_i_raw, -20.0, 20.0)
-                gk_j = jnp.clip(gk_j_raw, -20.0, 20.0)
-                clipmask_q = ((gq_i_raw >= -20.0) & (gq_i_raw <= 20.0)).astype(jnp.float32)
-                clipmask_k = ((gk_j_raw >= -20.0) & (gk_j_raw <= 20.0)).astype(jnp.float32)
-                eq_i = jnp.exp(gq_i)
-                ek_j = jnp.exp(gk_j)
-                q_scaled = q_i * eq_i
-                k_scaled = k_j * ek_j
-                bk_scaled = bk_i * eq_i
+            decay_diff = gc_i[:, None, :] - gc_j[None, :, :]
+            clipmask = ((decay_diff >= -20.0) & (decay_diff <= 20.0)).astype(jnp.float32)
+            edecay = jnp.exp(jnp.clip(decay_diff, -20.0, 20.0))
 
-                dq_scaled = scale * jnp.dot(dM_qk, k_scaled, precision=_HIGHEST)
-                dk_scaled_qk = scale * jnp.dot(dM_qk.T, q_scaled, precision=_HIGHEST)
-                dbk_scaled = jnp.dot(dM_kk, k_scaled, precision=_HIGHEST)
-                dk_scaled_kk = jnp.dot(dM_kk.T, bk_scaled, precision=_HIGHEST)
-                dk_scaled = dk_scaled_qk + dk_scaled_kk
+            L_qk = scale * q_i
+            R_qk = k_j
+            dL_qk = _dL_pair_sum(dM_qk, edecay, R_qk)
+            dR_qk = _dR_pair_sum(dM_qk, edecay, L_qk)
+            dgc_i_qk, dgc_j_qk = _dgc_pair_sum(dM_qk, edecay, L_qk, R_qk, clipmask)
 
-                dk_j_from_scaled = dk_scaled * ek_j
-                dgk_j = (dk_scaled * k_scaled) * clipmask_k
+            L_kk = bk_i
+            R_kk = k_j
+            dL_kk = _dL_pair_sum(dM_kk, edecay, R_kk)
+            dR_kk = _dR_pair_sum(dM_kk, edecay, L_kk)
+            dgc_i_kk, dgc_j_kk = _dgc_pair_sum(dM_kk, edecay, L_kk, R_kk, clipmask)
 
-                d_eq_i_total = dq_scaled * q_i + dbk_scaled * bk_i
-                dgq_i = (d_eq_i_total * eq_i) * clipmask_q
-                dq_i_from_scaled = dq_scaled * eq_i
-                dbk_i_from_scaled = dbk_scaled * eq_i   # d(b_i*k_i)
-
-                dq_ref[0, 0, 0, i0:i1] = clip_acc(dq_ref[0, 0, 0, i0:i1] + dq_i_from_scaled, config)
-                db_ref[0, 0, 0, i0:i1] = clip_acc(db_ref[0, 0, 0, i0:i1] + dbk_i_from_scaled, config)
-                dk_ref[0, 0, 0, j0:j1] = clip_acc(dk_ref[0, 0, 0, j0:j1] + dk_j_from_scaled, config)
-                dgc_ref[0, 0, 0, i0:i1] = clip_acc(dgc_ref[0, 0, 0, i0:i1] + dgq_i, config)
-                dgc_ref[0, 0, 0, j0:j1] = clip_acc(dgc_ref[0, 0, 0, j0:j1] - dgk_j, config)
-                # d(gq_i)/d(gn) = -1, d(gk_j)/d(gn) = +1 (within the clip
-                # window; clipmask_q/clipmask_k already zero the
-                # out-of-window terms consistently with dgq_i/dgk_j).
-                dgn_acc = dgn_acc + jnp.sum(dgk_j, axis=0) - jnp.sum(dgq_i, axis=0)
-            else:
-                decay_diff = gc_i[:, None, :] - gc_j[None, :, :]
-                clipmask = ((decay_diff >= -20.0) & (decay_diff <= 20.0)).astype(jnp.float32)
-                edecay = jnp.exp(jnp.clip(decay_diff, -20.0, 20.0))
-
-                L_qk = scale * q_i
-                R_qk = k_j
-                dL_qk = _dL_pair_sum(dM_qk, edecay, R_qk)
-                dR_qk = _dR_pair_sum(dM_qk, edecay, L_qk)
-                dgc_i_qk, dgc_j_qk = _dgc_pair_sum(dM_qk, edecay, L_qk, R_qk, clipmask)
-
-                L_kk = bk_i
-                R_kk = k_j
-                dL_kk = _dL_pair_sum(dM_kk, edecay, R_kk)
-                dR_kk = _dR_pair_sum(dM_kk, edecay, L_kk)
-                dgc_i_kk, dgc_j_kk = _dgc_pair_sum(dM_kk, edecay, L_kk, R_kk, clipmask)
-
-                dq_ref[0, 0, 0, i0:i1] = clip_acc(dq_ref[0, 0, 0, i0:i1] + dL_qk * scale, config)
-                db_ref[0, 0, 0, i0:i1] = clip_acc(db_ref[0, 0, 0, i0:i1] + dL_kk, config)
-                dk_ref[0, 0, 0, j0:j1] = clip_acc(dk_ref[0, 0, 0, j0:j1] + dR_qk + dR_kk, config)
-                dgc_ref[0, 0, 0, i0:i1] = clip_acc(dgc_ref[0, 0, 0, i0:i1] + dgc_i_qk + dgc_i_kk, config)
-                dgc_ref[0, 0, 0, j0:j1] = clip_acc(dgc_ref[0, 0, 0, j0:j1] + dgc_j_qk + dgc_j_kk, config)
-
-    if use_centering:
-        dgc_ref[0, 0, 0, n_mid] = clip_acc(dgc_ref[0, 0, 0, n_mid] + dgn_acc, config)
+            dq_ref[0, 0, 0, i0:i1] = clip_acc(dq_ref[0, 0, 0, i0:i1] + dL_qk * scale, config)
+            db_ref[0, 0, 0, i0:i1] = clip_acc(db_ref[0, 0, 0, i0:i1] + dL_kk, config)
+            dk_ref[0, 0, 0, j0:j1] = clip_acc(dk_ref[0, 0, 0, j0:j1] + dR_qk + dR_kk, config)
+            dgc_ref[0, 0, 0, i0:i1] = clip_acc(dgc_ref[0, 0, 0, i0:i1] + dgc_i_qk + dgc_i_kk, config)
+            dgc_ref[0, 0, 0, j0:j1] = clip_acc(dgc_ref[0, 0, 0, j0:j1] + dgc_j_qk + dgc_j_kk, config)
 
     dbk_final = db_ref[0, 0, 0]
     dk_final = dk_ref[0, 0, 0] + dbk_final * b_full
